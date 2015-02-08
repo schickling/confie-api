3fa8ab0d3fca13894032ab7c1bb1803816e96a00
Johannes Schickling <schickling.j@gmail.com>
2015-02-08 15:59:48 +0000

    api

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var _ = require('lodash');
 var express = require('express');
+var request = require('superagent');
 var cors = require('express-cors');
 var bodyParser = require('body-parser');
 var app = express();
@@ -17,8 +18,32 @@ var flatQuestions = _(questions)
     .value();
 
 var i;
-
-var answers = {};
+//var answers = {};
+var answers = {
+    first_name: '12',
+    last_name: 'wdfg',
+    product_type: 'my car',
+    marital_status: 'married',
+    title: 'mr',
+    date_of_birth: '2015-02-11T00:00:00.000Z',
+    postcode: 'sdf',
+    email: 'sdf',
+    phone: 'dsf',
+    employment_type: 'employed',
+    vehicle_registration_number: 'sdf',
+    vehicle_approximate_value: 'sdf',
+    vehicle_registrerd_owner: 'am',
+    vechile_overnight_location: 'Garaged',
+    vehicle_use: 'Social, domestic & pleasure only',
+    vehicle_miles_travelled_this_year: 'dsf',
+    driving_license_type: 'UK Full',
+    driving_license_age: 'dsf',
+    number_of_motor_claims: 'dsf',
+    number_of_motor_convictions: 'sdf',
+    no_claims_years: 'dsf',
+    vehicle_cover_start_date: '2015-02-04T00:00:00.000Z'
+};
+var endpoints;
 
 function isLast(id) {
     return _.findWhere(flatQuestions, {
@@ -54,6 +79,30 @@ app.post('/answer', function(req, res) {
         i++;
     }
     res.send(result);
+});
+
+var api = 'https://7d777aa7.ngrok.com/';
+
+app.post('/submit', function(req, res) {
+    request
+        .post(api + 'job')
+        .send(answers)
+        .end(function(res) {
+            // Do something
+            console.log(res);
+            endpoints = res.body.result_endpoints;
+        });
+});
+
+app.get('/results', function(req, res) {
+    var endpoint = api + endpoints[0];
+    console.log(endpoints);
+    request
+        .get(endpoint)
+        .end(function(res) {
+            // Do something
+            console.log(res);
+        });
 });
 
 app.listen(8000);
