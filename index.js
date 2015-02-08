e0ad3e17a09be1de1120f72ef97dea710be30006
Johannes Schickling <schickling.j@gmail.com>
2015-02-08 09:41:27 +0000

    questions

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 var express = require('express');
 var cors = require('express-cors');
 var app = express();
+var questions = require('./questions.json');
+
+var i = 1;
 
 app.use(cors({
     allowedOrigins: ['localhost:*']
 }));
 
 app.get('/initial', function(req, res) {
-    var mock = require('./initial.json');
-    res.send(mock);
+    res.send({
+        question: questions[0],
+        progress: 0,
+    });
 });
 
-app.post('/answer', function(req, res) {
-    var mock = require('./response.json');
-    res.send(mock);
+app.post('/answer/:id', function(req, res) {
+    res.send({
+        question: questions[i],
+        progress: i / 100,
+    });
+    i++;
 });
 
 app.listen(8000);
