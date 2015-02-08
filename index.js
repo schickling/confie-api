bc252da977d66296241faa86c9e59deb23528d0b
Johannes Schickling <schickling.j@gmail.com>
2015-02-08 11:41:07 +0000

    g

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
+var _ = require('lodash');
 var express = require('express');
 var cors = require('express-cors');
+var bodyParser = require('body-parser');
 var app = express();
 var questions = require('./questions.json');
+var flatQuestions = _(questions)
+    .map(function(qs) {
+        return qs.map(function(q, i) {
+            return {
+                id: q.id,
+                isLast: i === qs.length - 1
+            }
+        })
+    })
+    .flatten()
+    .value();
 
 var i = 1;
 
 var answers = {};
 
+function isLast(id) {
+    return _.findWhere(flatQuestions, {
+        id: id
+    }).isLast;
+}
+
 app.use(cors({
     allowedOrigins: ['localhost:*']
 }));
+
+app.use(bodyParser.json());
 
 app.get('/initial', function(req, res) {
     res.send({
@@ -18,11 +39,17 @@ app.get('/initial', function(req, res) {
     });
 });
 
-app.post('/answer/:id', function(req, res) {
-    res.send({
-        questions: questions[i],
+app.post('/answer', function(req, res) {
+    answers[req.body.id] = req.body.value;
+
+    var result = {
         progress: i / questions.length,
-    });
+    };
+    console.log(isLast(req.body.id));
+    if (isLast(req.body.id)) {
+        result.questions = questions[i];
+    }
+    res.send(result);
     i++;
 });
 
